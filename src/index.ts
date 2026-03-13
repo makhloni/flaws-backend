cc0624ec05dff93c4e26511544098129a9580d83
unknown <andile593>
2026-03-13 19:48:20 +0200

    removing the localhost url

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000
 
 app.use(cors({
   origin: [
-    'http://localhost:5173',
+    'https://flaws-production.up.railway.app/',
     'https://flaws-frontend.vercel.app/', 
   ],
   credentials: true,
